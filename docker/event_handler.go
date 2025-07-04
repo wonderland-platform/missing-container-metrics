8abbbb3aeb3d04ad33d888de166ab5245d639981
Heiko Voigt <heiko.voigt@jimdo.com>
2025-07-04 15:26:48 +0200

    chore(repo): update repository references for this repo

--- docker/event_handler.go
+++ docker/event_handler.go
@@ -7,9 +7,9 @@ import (
 	"time"
 
 	"github.com/docker/docker/api/types/events"
-	"github.com/draganm/missing-container-metrics/metrics"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/wonderland-platform/missing-container-metrics/metrics"
 )
 
 type container struct {
